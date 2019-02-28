ffdae95769b6cfe8f40f5e6281e0a46201b5d155
Stefany2210 <stefany.liseth@gmail.com>
2019-02-28 12:08:56 -0500

    cifrado y descifrado ok

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,28 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (offset,string) => {
+    let nuevoMensaje ="";
+    for (let i = 0; i < string.length; i++) {
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        let numberAscii =((string[i].charCodeAt())-65+offset)%26+65;
+        let lettercipher = String.fromCharCode(numberAscii);
+        nuevoMensaje= nuevoMensaje+lettercipher;
+      } else {
+        nuevoMensaje = nuevoMensaje+string[i];
+      }
+    }
+    return nuevoMensaje;
   },
-  decode: () => {
-    /* Acá va tu código */
-  }
-};
+  decode: (offset,string) => {
+    let nuevoMensaje1 ="";
+    for (let i = 0; i < string.length; i++) {
+      if (string[i].charCodeAt()>=65 && string[i].charCodeAt()<=90){
+        let numberAscii =((string[i].charCodeAt())-90-offset)%26+90;
+        let lettercipher = String.fromCharCode(numberAscii);
+        nuevoMensaje1= nuevoMensaje1+lettercipher;
+      } else {
+        nuevoMensaje1 = nuevoMensaje1+string[i];
+      }
+    }
+    return nuevoMensaje1;
+  },
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-/* Acá va tu código */
+const encriptar = document.getElementById("encriptar"); // id=único de un elemento, si cambia el nombre no lo encuentra
+const frase = document.getElementById("frase");
+const textoCifrado = document.getElementById("texto-cifrado");
+const movimiento = document.getElementById("movimiento");
+const descifrar = document.getElementById("descifrar");
+const textoDescifrado = document.getElementById("texto-descifrado");
+
+const clikEncriptar =()=>{
+    let mensaje =frase.value;
+    let numero =parseInt(movimiento.value);
+    let nuevoMensaje =cipher.encode(numero,mensaje);
+    return textoCifrado.value = nuevoMensaje;
+}
+
+encriptar.addEventListener("click",clikEncriptar)
+
+const clikDesencriptar =()=>{
+    let mensaje =frase.value;
+    let numero =parseInt(movimiento.value);
+    let nuevoMensaje1 =cipher.decode(numero,mensaje);
+    return textoDescifrado.value = nuevoMensaje1;
+}
+
+descifrar.addEventListener("click",clikDesencriptar)
+
+
+
+
+/*
+encriptar.addEventListener("click", () => {
+textoCifrado.innerHTML = frase.value
+console.log(movimiento.value)
+})
+
+descifrar.addEventListener("click", ()=> {
+    textoDescifrado.innerHTML = frase.value
+    console.log(movimiento.value)
+}) */
+
